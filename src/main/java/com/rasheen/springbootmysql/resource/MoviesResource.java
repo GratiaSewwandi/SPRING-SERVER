23f269f612a2cd6fdd90e29734d4fe152204d940
Rasheen Ruwisha <sheen.ruwisha@gmail.com>
2018-07-13 12:14:57 +0530

    New : Delete Function

--- src/main/java/com/rasheen/springbootmysql/resource/MoviesResource.java
+++ src/main/java/com/rasheen/springbootmysql/resource/MoviesResource.java
@@ -3,6 +3,8 @@ package com.rasheen.springbootmysql.resource;
 import com.rasheen.springbootmysql.model.Movie;
 import com.rasheen.springbootmysql.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,5 +31,14 @@ public class MoviesResource {
     public List<Movie> delete(@PathVariable("id") int id){
         movieRepository.deleteById(id);
         return movieRepository.findAll();
+    }
+
+
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Movie> updateEmployee(@PathVariable("id") int id, @RequestBody Movie movie)
+    {
+        System.out.println(id);
+        System.out.println(movie);
+        return new ResponseEntity<Movie>(movie, HttpStatus.OK);
     }
 }
