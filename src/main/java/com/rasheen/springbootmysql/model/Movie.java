120d73841d0906fa2b5f9a07d35fefd8a8f9ef58
Rasheen Ruwisha <sheen.ruwisha@gmail.com>
2018-07-17 11:10:16 +0530

    Fix:Changed the add form

--- src/main/java/com/rasheen/springbootmysql/model/Movie.java
+++ src/main/java/com/rasheen/springbootmysql/model/Movie.java
@@ -22,6 +22,27 @@ public class  Movie {
     @Column(name ="poster_url")
     private String poster_url;
 
+    @Column(name ="short_desc")
+    private String short_desc;
+
+    @Column(name ="description")
+    private String description;
+
+    @Column(name ="director")
+    private String director;
+
+    @Column(name ="country")
+    private String country;
+
+    @Column(name ="languages")
+    private String languages;
+
+    @Column(name ="budget")
+    private String budget;
+
+    @Column(name ="url")
+    private String url;
+
     public Movie(){
 
     }
@@ -72,5 +93,62 @@ public class  Movie {
 
     public void setPoster_url(String poster_url) {
         this.poster_url = poster_url;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String languages) {
+        this.languages = languages;
+    }
+
+    public String getBudget() {
+        return budget;
+    }
+
+    public void setBudget(String budget) {
+        this.budget = budget;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getShort_desc() {
+        return short_desc;
+
+    }
+
+    public void setShort_desc(String short_desc) {
+        this.short_desc = short_desc;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
